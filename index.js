9b94889f312997de8b3a716f1b96d0992101b974
Favour Ifurukpe <tinafavour2018@gmail.com>
2024-07-14 18:18:39 +0100

    main

--- index.js
+++ index.js
@@ -42,14 +42,14 @@ const replaceTemplate = (temp, product) => {
   }
   
 };
-const tempOverview = fs.readFileSync(`${__dirname}/starter/templates/template-overview.html`,"utf-8"
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`,"utf-8"
 );
-const tempCard = fs.readFileSync(`${__dirname}/starter/templates/template-card.html`,"utf-8"
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,"utf-8"
 );
-const tempProduct = fs.readFileSync(`${__dirname}/starter/templates/product.html`,"utf-8"
+const tempProduct = fs.readFileSync(`${__dirname}/templates/product.html`,"utf-8"
 );
 
-const data = fs.readFileSync(`${__dirname}/starter/dev-data/data.json`,"utf-8"
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,"utf-8"
 );
 const dataObj = JSON.parse(data);
 
